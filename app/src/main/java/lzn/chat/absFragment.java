449fb5856b85d26815ee855a3ab299833cc45af8
Allen <506841842@qq.com>
2016-05-24 17:48:18 +0800

    commit

---
+++ app/src/main/java/lzn/chat/absFragment.java
@@ -0,0 +1,17 @@
+package lzn.chat;
+
+import android.app.Fragment;
+import android.os.Bundle;
+
+/**
+ * Created by Allen on 2016/5/24.
+ */
+public abstract class absFragment  extends Fragment {
+    protected myApplication mvApplication;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mvApplication = myApplication.getInstance();
+    }
+}
---
+++ app/src/main/java/lzn/chat/main/item/contactItem/addFriend/addFriendActivity.java
@@ -0,0 +1,48 @@
+package lzn.chat.main.item.contactItem.addFriend;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import lzn.chat.R;
+
+/**
+ * Created by Allen on 2016/5/24.
+ */
+public class addFriendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_friend_layout);
+        initTooBar();
+    }
+
+    private void initTooBar() {
+        Toolbar lvToolbar = (Toolbar) this.findViewById(R.id.toolbar);
+        if(lvToolbar != null)
+        {
+            setSupportActionBar(lvToolbar);
+            View lvView = LayoutInflater.from(this).inflate(R.layout.add_friend_input_layout,null);
+            lvView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            lvToolbar.addView(lvView);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_friend_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
